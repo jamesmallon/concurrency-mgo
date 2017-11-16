933a8285cfa0cf536115ec431f34510e13e642c8
Thiago Mallon <thiagomallon@gmail.com>
2017-11-15 22:29:48 -0800

    Updating database routines

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"userv/commons/database"
+)
+
 func main() {
+	// Get Mongodb instance connection
+	mongoSession := database.ConnMongo()
+
+	// Create a wait group to manage the goroutines.
+	var waitGroup sync.WaitGroup
+
+	// Perform 10 concurrent queries against the database.
+	waitGroup.Add(1)
+	go mongoSession.RunQuery(&waitGroup)
+
+	// Wait for all the queries to complete.
+	waitGroup.Wait()
+	fmt.Println("All Queries Completed")
 
 }
---
+++ modules/delivery/models/CarModel.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+type (
+	Delivery struct {
+		Id       bson.ObjectId `bson:"_id,omitempty"`
+		IpPort   string        `bson:"ipPort"`
+		Protocol string        `bson:"protocol"`
+	}
+)
