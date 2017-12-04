8e3fef2f8aab433b2f7be7ce2bb1228c39231a83
Thiago Mallon <johnthegreenobrien@gmail.com>
2017-12-04 06:15:54 -0800

    Updating redis and mongodb flow

--- modules/dailyDelivery/controllers/DeliveryController.go
+++ modules/dailyDelivery/controllers/DeliveryController.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	//"sync"
+	"userv/commons/cache"
 	"userv/commons/database"
 	"userv/modules/dailyDelivery/dao"
 )
@@ -14,28 +16,28 @@ import (
  */
 type deliveryController struct {
 	mSession *database.MongoSession
+	rClient  *cache.RedisClient
 }
 
 /**
  *
  */
-func DeliveryController(mongoSession *database.MongoSession) *deliveryController {
-	return &deliveryController{mongoSession}
+func DeliveryController(mongoSession *database.MongoSession, redisClient *cache.RedisClient) *deliveryController {
+	return &deliveryController{mongoSession, redisClient}
 }
 
 /**
- *
+ * @method GetDelivery
  */
 func (uc *deliveryController) GetDelivery(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	deliveryDao := dao.NewDeliveryDao()
-
 	delivery, _ := deliveryDao.GetDelivery(uc.mSession)
 	delivery, _ = deliveryDao.IncrementField(uc.mSession, "sussDlry", delivery)
 	fmt.Println(delivery)
 
 	deliveryCacheDao := dao.NewDeliveryCacheDao()
-	deliveryCacheDao.SettingKey("mingal")
-	fmt.Println(deliveryCacheDao.GettingKey("mingal"))
+	deliveryCacheDao.SettingKey("trust", "+1000000 4U", uc.rClient)
+	fmt.Println(deliveryCacheDao.GettingKey("trust", uc.rClient))
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
